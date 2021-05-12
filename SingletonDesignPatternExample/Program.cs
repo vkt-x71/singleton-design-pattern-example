1e53dab79e44867cc5c0cd4a39a568a76fc37260
vkt571 <veyselkaranitekgoz@gmail.com>
2021-05-12 15:25:55 +0300

    working single thread app

---
+++ SingletonDesignPatternExample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SingletonDesignPatternExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Validation validation = Validation.Instance;
+            validation.EmailRegex = "[A-Za-z0-9]+";
+            Validation validation2 = Validation.Instance;
+            Console.WriteLine($"Test text {validation2.EmailRegex}");
+            Console.Read();
+        }
+    }
+}
---
+++ SingletonDesignPatternExample/Validation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDesignPatternExample
+{
+    public class Validation
+    {
+
+        private static Validation _instance;
+        public static Validation Instance
+        {
+            get
+            {
+                if (_instance == null) _instance = new Validation();
+                return _instance;
+            }
+        }
+        private Validation() { }
+
+        public string EmailRegex { get; set; }
+        public bool IsMailValid(string mail)
+        {// fake method
+            if (mail.Contains("@")) return true;
+            else return false;
+        }
+        public bool IsPhoneValid(string phone)
+        {// fake method
+            if (phone.Contains("0")) return true;
+            else return false;
+        }
+    }
+}
